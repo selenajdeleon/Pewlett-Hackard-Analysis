06fd225342de6fe0ebced6f4a5415a017f4d7a1c
Selena de Leon <50776370+selenajdeleon@users.noreply.github.com>
2021-01-13 01:45:23 -0800

    fix schema 1

--- schema.sql
+++ schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE dept_manager (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE salaries (
@@ -44,6 +44,9 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no)
 );
+
+
+--CREATE TABLE dept_emp(emp_no INT NOT NULL,dept_no VARCHAR(4) NOT NULL,from_date DATE NOT NULL,to_date DATE NOT NULL,PRIMARY KEY (emp_no, dept_no),FOREIGN KEY (emp_no) REFERENCES employees (emp_no),FOREIGN KEY (dept_no) REFERENCES departments (dept_no));
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
