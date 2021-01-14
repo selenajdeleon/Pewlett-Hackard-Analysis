b259bd9241221370e6ed6e6c5c14dae2f44852e8
Selena de Leon <50776370+selenajdeleon@users.noreply.github.com>
2021-01-13 19:55:28 -0800

    updated schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
 -- Creating tables for PH-EmployeeDB
+
+
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -25,7 +27,7 @@ CREATE TABLE dept_manager (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 
@@ -49,16 +51,6 @@ CREATE TABLE dept_emp (
 	PRIMARY KEY (emp_no, dept_no)
 );
 
-
---CREATE TABLE dept_emp(
-    --emp_no INT NOT NULL,
-    --dept_no VARCHAR(4) NOT NULL,
-    --from_date DATE NOT NULL,
-    --to_date DATE NOT NULL,
-    --PRIMARY KEY (emp_no, dept_no),
-    --FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    --FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
-    --);
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
