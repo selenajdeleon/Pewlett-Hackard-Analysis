f798b47c466ed85757b484951a03b4804f434dc1
Selena de Leon <50776370+selenajdeleon@users.noreply.github.com>
2021-01-13 19:05:36 -0800

    correct schema

--- schema.sql
+++ schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
+
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
@@ -15,6 +16,7 @@ CREATE TABLE employees (
 	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no)
 );
+
 
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
@@ -26,6 +28,7 @@ CREATE TABLE dept_manager (
 	PRIMARY KEY (emp_no)
 );
 
+
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -35,6 +38,7 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
@@ -42,11 +46,19 @@ CREATE TABLE dept_emp (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 
---CREATE TABLE dept_emp(emp_no INT NOT NULL,dept_no VARCHAR(4) NOT NULL,from_date DATE NOT NULL,to_date DATE NOT NULL,PRIMARY KEY (emp_no, dept_no),FOREIGN KEY (emp_no) REFERENCES employees (emp_no),FOREIGN KEY (dept_no) REFERENCES departments (dept_no));
+--CREATE TABLE dept_emp(
+    --emp_no INT NOT NULL,
+    --dept_no VARCHAR(4) NOT NULL,
+    --from_date DATE NOT NULL,
+    --to_date DATE NOT NULL,
+    --PRIMARY KEY (emp_no, dept_no),
+    --FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    --FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+    --);
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
@@ -54,7 +66,5 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
-
-SELECT * FROM departments;
