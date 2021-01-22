aa3e3c169d6f4e56837747f3d516794786d531e3
Selena de Leon <50776370+selenajdeleon@users.noreply.github.com>
2021-01-21 20:30:29 -0800

    M7 Challenge start

---
+++ M7-Challenge.sql
@@ -0,0 +1,11 @@
+-- Pewlett Hackard Retirees Challenge (Module 7)
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+-- INTO retirees_title
+FROM _______
+ORDER BY _____, _____ DESC;
