8c4b9aa0217e6a95a04866b71aef2ec3f9b34b50
Selena de Leon <50776370+selenajdeleon@users.noreply.github.com>
2021-01-22 01:29:19 -0800

    update on D1

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -15,16 +15,32 @@ ON (e.emp_no = ti.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no ASC;
 
+-- -- Use Dictinct with Orderby to remove duplicate rows
+-- SELECT DISTINCT ON (emp_no) e.emp_no,
+-- e.first_name,
+-- e.last_name,
+-- ti.title,
+-- ti.from_date,
+-- ti.to_date
+-- -- INTO unique_titles
+-- FROM employees as e
+-- LEFT JOIN titles as ti
+-- ON (e.emp_no = ti.emp_no)
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- ORDER BY emp_no, e.emp_no ASC;
+
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) e.emp_no,
-e.first_name,
-e.last_name,
-ti.title,
+SELECT DISTINCT ON (emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title,
 ti.from_date,
 ti.to_date
 -- INTO unique_titles
-FROM employees as e
-LEFT JOIN titles as ti
-ON (e.emp_no = ti.emp_no)
+FROM retirement_titles as rt
+INNER JOIN titles as ti
+ON (rt.emp_no = ti.emp_no)
+INNER JOIN employees as e
+ON (e.emp_no = rt.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY emp_no, e.emp_no ASC;
+ORDER BY emp_no, rt.emp_no ASC, to_date, ti.to_date DESC;
