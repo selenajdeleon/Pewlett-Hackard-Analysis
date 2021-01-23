0f6d5713e0e502304e4ae5133a8207978597f34b
Selena de Leon <50776370+selenajdeleon@users.noreply.github.com>
2021-01-22 21:51:20 -0800

    M7 D1 complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -2,6 +2,7 @@
 
 -- D1:
 
+-- Create table for retiring employees by title
 SELECT e.emp_no,
 e.first_name,
 e.last_name,
@@ -15,50 +16,18 @@ ON (e.emp_no = ti.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no ASC;
 
--- -- Use Dictinct with Orderby to remove duplicate rows
--- SELECT DISTINCT ON (emp_no) e.emp_no,
--- e.first_name,
--- e.last_name,
--- ti.title,
--- ti.from_date,
--- ti.to_date
--- -- INTO unique_titles
--- FROM employees as e
--- LEFT JOIN titles as ti
--- ON (e.emp_no = ti.emp_no)
--- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
--- ORDER BY emp_no, e.emp_no ASC;
-
--- -- Use Dictinct with Orderby to remove duplicate rows
--- SELECT DISTINCT ON (emp_no) rt.emp_no,
--- rt.first_name,
--- rt.last_name,
--- rt.title,
--- ti.from_date,
--- ti.to_date
--- -- INTO unique_titles
--- FROM retirement_titles as rt
--- INNER JOIN titles as ti
--- ON (rt.emp_no = ti.emp_no)
--- INNER JOIN employees as e
--- ON (e.emp_no = rt.emp_no)
--- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
--- ORDER BY emp_no, rt.emp_no ASC, to_date, ti.to_date DESC;
-
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT * FROM (
-    SELECT DISTINCT ON (emp_no) rt.emp_no,
-    rt.first_name,
-    rt.last_name,
-    rt.title,
-    ti.from_date, 
-    ti.to_date
-    -- INTO unique_titles
-    FROM retirement_titles as rt
-    INNER JOIN titles as ti
-    ON (rt.emp_no = ti.emp_no)
-    INNER JOIN employees as e
-    ON (e.emp_no = rt.emp_no)
-    WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-    ) k
-ORDER BY emp_no ASC, to_date DESC;
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no ASC, rt.to_date DESC;
+
+-- Create table to count # of retiring employees by title
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
